237034eb6488a6cc41d0459bfe8b0151d93f5b41
Istrate-Mihai <i.mihai9960@gmail.com>
2025-01-23 09:53:13 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    // A simple calculator class in Java
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.subtract(45, 11));
+    }
+
+    public Calculator() {}
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int modulo(int a, int b) {
+        return a % b;
+    }
+}
